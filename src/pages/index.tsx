48d70007c695d7a45168860c1f5094e168f86f46
brenoos <breno.oliver07@gmail.com>
2021-08-26 14:15:14 -0300

    feat(login): Login Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,64 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import {
+  Button,
+  Flex,
+  FormLabel,
+  Input,
+  Stack,
+  FormControl,
+} from "@chakra-ui/react";
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-   <h1>dashgo</h1>
-  )
-}
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
 
-export default Home
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              id="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+          <Button type="submit" mt="6" colorScheme="pink" size="lg">
+            Login
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Home;
