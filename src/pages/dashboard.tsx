eb71dd70e7c87b8508e525aa996f278fe98405a9
brenoos <breno.oliver07@gmail.com>
2021-08-30 16:54:26 -0300

    feat: Sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,19 @@
+import { Flex } from "@chakra-ui/react";
 import { NextPage } from "next";
+import React from "react";
 import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 const Dashboard: NextPage = () => {
-  return <Header></Header>;
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Dashboard;
