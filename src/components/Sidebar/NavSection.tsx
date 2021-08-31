3aea3291ed1e3ea6df92bc8844368d950f70e4e2
brenoos <breno.oliver07@gmail.com>
2021-08-31 14:58:25 -0300

    fix(NavSection): Implementing stack for spacing

--- src/components/Sidebar/NavSection.tsx
+++ src/components/Sidebar/NavSection.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Stack, Text } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 interface NavSectionProps {
@@ -12,7 +12,9 @@ export default function NavSection({ title, children }: NavSectionProps) {
       <Text fontWeight="bold" color="gray.400" fontSize="small">
         {title}
       </Text>
-      {children}
+      <Stack spacing="4" mt="8" align="stretch">
+        {children}
+      </Stack>
     </Box>
   );
 }
