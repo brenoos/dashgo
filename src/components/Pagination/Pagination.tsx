4b77297e3bb383f7b2b6f5b07bf523b6b8e6fd8e
brenoos <breno.oliver07@gmail.com>
2021-08-31 15:20:55 -0300

    chore(Pagination): Spliting in components

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Stack } from "@chakra-ui/react";
 import React from "react";
+import PaginationItem from "./PaginationItem";
 
 export function Pagination() {
   return (
@@ -14,52 +15,12 @@ export function Pagination() {
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
       <Stack direction="row" spacing="2">
-        <Button
-          size="sm"
-          fontSize="xs"
-          width="4"
-          colorScheme="pink"
-          disabled
-          _disabled={{
-            bg: "pink.500",
-            cursor: "default",
-          }}
-        >
-          1
-        </Button>
-        <Button
-          size="sm"
-          fontSize="xs"
-          width="4"
-          bg="gray.700"
-          _hover={{
-            bg: "gray.500",
-          }}
-        >
-          2
-        </Button>
-        <Button
-          size="sm"
-          fontSize="xs"
-          width="4"
-          bg="gray.700"
-          _hover={{
-            bg: "gray.500",
-          }}
-        >
-          3
-        </Button>
-        <Button
-          size="sm"
-          fontSize="xs"
-          width="4"
-          bg="gray.700"
-          _hover={{
-            bg: "gray.500",
-          }}
-        >
-          4
-        </Button>
+        <PaginationItem pageNumber={1} isCurrent />
+        <PaginationItem pageNumber={2} />
+        <PaginationItem pageNumber={3} />
+        <PaginationItem pageNumber={4} />
+        <PaginationItem pageNumber={5} />
+        <PaginationItem pageNumber={6} />
       </Stack>
     </Stack>
   );
