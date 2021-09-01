a0d0e4149185e63f52b4fa64ca84cca161ba496b
brenoos <breno.oliver07@gmail.com>
2021-09-01 16:31:10 -0300

    feat(dashboard) : Responsive

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -62,13 +62,13 @@ const Dashboard: NextPage = () => {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               This week subscribers
             </Text>
             <Chart type="area" height={160} options={options} series={series} />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Opening rate
             </Text>
